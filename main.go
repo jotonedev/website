6a3f390cc4cdc9bf840140acaf091cd6ddade37b
John Toniutti <john.toniutti@gmail.com>
2023-01-17 18:36:20 +0100

    Improve templates

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"embed"
+	"github.com/gin-contrib/cors"
+	limits "github.com/gin-contrib/size"
 	log "github.com/sirupsen/logrus"
 	"jotone.eu/database"
 	"jotone.eu/routers"
+	"time"
 )
 
 //go:embed templates/*
@@ -17,6 +20,15 @@ func main() {
 	database.ConnectDB()
 
 	router := routers.InitRouter(tmplFS, staticFS)
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://jotone.eu", "https://images.jotone.eu"},
+		AllowMethods: []string{"GET", "HEAD"},
+		AllowHeaders: []string{"*"},
+		MaxAge:       12 * time.Hour,
+	}))
+
+	router.Use(limits.RequestSizeLimiter(10))
 
 	err := router.Run("localhost:8080")
 	if err != nil {
